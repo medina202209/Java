0fb2e64ffebba416bdeb46411afa4ae0ed5c9c44
Alexis Medina <alexismedina_87@hotmail.com>
2024-09-30 14:36:28 -0500

    Ajuste programa

--- src/poo/ComisionVentas.java
+++ src/poo/ComisionVentas.java
@@ -6,7 +6,10 @@ public class ComisionVentas {
 
     //arreglo ventas
     int ventas[];
-    
+
+    int salario;
+    //int comision;
+    //int total;
 
     //constructor
     public ComisionVentas(int[] ventas) {
@@ -21,18 +24,53 @@ public class ComisionVentas {
         this.ventas = ventas;
     }
 
-    //metodo para validar salarios
-    public void Comision() {
+    public void salario() {
+
+        int[] contenoEmpleados = new int[9];
 
         for (int i = 0; i < ventas.length; i++) {
 
-            int comision = ventas[i] * 9 / 100 + 200;
+            salario = getVentas()[i] * 9 / 100 + 200;
+            
+            System.out.printf("EL salario del vendedor %d:", i + 1);
+            System.out.println(salario);
 
-           int [] comisiones = new int[]{comision};
+            if (salario >= 200 && salario < 300) {
+                contenoEmpleados[0]++;
+            } else if (salario >= 300 && salario < 400) {
+                contenoEmpleados[1]++;
+            } else if (salario >= 400 && salario < 500) {
+                contenoEmpleados[2]++;
+            } else if (salario >= 500 && salario < 600) {
+                contenoEmpleados[3]++;
+            } else if (salario >= 600 && salario < 700) {
+                contenoEmpleados[4]++;
+            } else if (salario >= 700 && salario < 800) {
+                contenoEmpleados[5]++;
+            } else if (salario >= 800 && salario < 900) {
+                contenoEmpleados[6]++;
+            } else if (salario >= 900 && salario < 1000) {
+                contenoEmpleados[7]++;
+            } else if (salario >= 1000) {
+                contenoEmpleados[8]++;
+            }
 
-           System.out.println("vendedor " + i + "1" + Arrays.toString(comisiones));
         }
+
+        System.out.println("\nSalarios             # Vendedores");
+        System.out.printf("$200 - $299          | %d%n", contenoEmpleados[0]);
+        System.out.printf("$300 - $399          | %d%n", contenoEmpleados[1]);
+        System.out.printf("$400 - $499          | %d%n", contenoEmpleados[2]);
+        System.out.printf("$500 - $599          | %d%n", contenoEmpleados[3]);
+        System.out.printf("$600 - $699          | %d%n", contenoEmpleados[4]);
+        System.out.printf("$700 - $799          | %d%n", contenoEmpleados[5]);
+        System.out.printf("$800 - $899          | %d%n", contenoEmpleados[6]);
+        System.out.printf("$900 - $999          | %d%n", contenoEmpleados[7]);
+        System.out.printf("$1,000 en adelante   | %d%n", contenoEmpleados[8]);
         
+        System.out.println("---------------------------------------");
+        System.out.println("Arreglo contador");
+        System.out.println("" + Arrays.toString(contenoEmpleados));
 
     }
 
--- src/poo/MainComisionVentas.java
+++ src/poo/MainComisionVentas.java
@@ -7,22 +7,26 @@ public class MainComisionVentas {
 
     public static void main(String[] args) {
 
-        int[] ventas = new int[5];
+        int[] ventas = new int[8];
 
         ComisionVentas Ventas = new ComisionVentas(ventas);
 
         Scanner teclado = new Scanner(System.in);
 
         //ingresar valores de las ventas por teclado
-        for (int i = 0; i < ventas.length; i++) {
+        for (int i = 0 ; i < ventas.length; i++) {
+
             System.out.printf("Ingresa venta del vendedor %d: ", i + 1);
+
             ventas[i] = teclado.nextInt();
         }
 
         //mostrar arreglo con las ventas
-        System.out.println("" + Arrays.toString(ventas));
-        
-        Ventas.Comision();
+        System.out.println("---------------------------------------");
+        System.out.println("Arreglo de ventas:\n" + Arrays.toString(ventas));
+
+        System.out.println("---------------------------------------");
+        Ventas.salario();
 
     }
 }
